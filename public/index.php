7a3d39d1ed60220f2df46705428fb6ccdc1d2be8
Douglas Silva <doug.hs@protonmail.ch>
2020-11-07 17:19:12 -0300

    Catch RouteNotFoundException

--- public/index.php
+++ public/index.php
@@ -9,14 +9,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Fragments\Component\Http\Request;
 use Fragments\Component\Http\Response;
 use Fragments\Component\Routing\Router;
-use Fragments\Bundle\Exception\NotFoundHttpException;
+use Fragments\Component\Routing\Exception\RouteNotFoundException;
 
 $request = new Request();
 
 try {
     $router = new Router();
     $response = $router->run($request);
-} catch (NotFoundHttpException $e) {
+} catch (RouteNotFoundException $e) {
     $response = new Response('Page not found', 404);
 } catch (Throwable $e) {
     error_log($e);
