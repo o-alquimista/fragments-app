75d6e0c2d53bd32f7bde0640475bdcfc8e027672
Douglas Silva <doug.hs@protonmail.ch>
2020-11-16 00:08:33 -0300

    Updated the front controller and removed instances of HttpException

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -8,28 +8,28 @@ use App\Entity\User;
 class UserRepository
 {
     private $pdo;
-    
+
     public function __construct()
     {
         $this->pdo = PdoConnection::getConnection();
     }
-    
+
     public function getUserByUsername(string $username): User
     {
         $stmt = $this->pdo->prepare("SELECT * FROM user WHERE username = :username");
         $stmt->bindValue(':username', $username, \PDO::PARAM_STR);
         $stmt->execute();
         $row = $stmt->fetch();
-        
+
         if (!$row) {
-            throw new \Exception('User not found.');
+            throw new \Exception('User not found.', 404);
         }
-        
+
         $user = new User();
         $user->setId($row['id']);
         $user->setUsername($row['username']);
         $user->setPassword($row['password']);
-        
+
         return $user;
     }
 }
